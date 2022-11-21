bfe42a3da43b0c4488fde8cace2659cc10e100dc
rodolfoantonio <rodolfoantonio.muriel512@comunidadunir.net>
2022-11-21 01:14:31 -0500

    la base h2 no se lleva bien con tablas cuyos primary key auto esten llenas, se recomienda que esté vacia y generatedvalue se identity o sin argumentos

--- src/main/java/plataformas/demo/modelos/PeliculaAlquilada.java
+++ src/main/java/plataformas/demo/modelos/PeliculaAlquilada.java
@@ -30,7 +30,7 @@ import lombok.ToString;
 public class PeliculaAlquilada {
 
         @Id
-        @GeneratedValue(strategy=GenerationType.IDENTITY)
+        @GeneratedValue
         private long idAlquilada;
 
         @ManyToOne
