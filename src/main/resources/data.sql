8fecd2e97946fba1453e64c0bb57c836ef7aba24
rodolfoantonio <rodolfoantonio.muriel512@comunidadunir.net>
2022-11-24 21:40:25 -0500

    se agregan data.sql con pelis reales y conexión a aws

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,12 @@
 INSERT INTO PELICULA_CATALOGO VALUES
 
-(1, 'espanol', 'muy buena', 'poster', 'fecha', 'titulo', '3/5'),
+(436270, 'en', 'Nearly 5,000 years after he was bestowed with the almighty powers of the Egyptian gods—and imprisoned just as quickly—Black Adam is freed from his earthly tomb, ready to unleash his unique form of justice on the modern world.', '/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg', '2022-10-19', 'Black Adam', '7.1'),
 
-(2, 'espanol', 'muy buena', 'poster', 'fecha', 'titulo', '3/5'),
+(1013860, 'en', 'When Sheriff Roy Pulsipher finds himself in the afterlife, he joins a special police force and returns to Earth to save humanity from the undead.', '/g4yJTzMtOBUTAR2Qnmj8TYIcFVq.jpg', '2022-11-15', 'R.I.P.D. 2: Rise of the Damned', '6.9');
 
-(3, 'espanol3', 'muy buena 3', 'poster3', 'fecha3', 'titulo3', '3/5 3'),
-
-(4, 'espanol4', 'muy buena 4', 'poster4', 'fecha4', 'titulo4', '3/5 4');
 
 INSERT INTO USUARIO VALUES
 
-('admin', 'nombreadmin', 'admin', 'ADMIN'),
+('admin@hotmail.com', 'nombreadmin', 'admin', 'ADMIN'),
 
-('user', 'nombreUser', 'user', 'USER');
+('user@hotmail.com', 'nombreUser', 'user', 'USER');
